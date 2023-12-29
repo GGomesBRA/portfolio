b9e067786186caf3b4b3b219c0255e73925fbed0
GABRIEL RIBEIRO DE OLIVEIRA GOMES <m170488@smb.saneago.com.br>
2023-12-29 16:21:14 -0300

    created apps

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
