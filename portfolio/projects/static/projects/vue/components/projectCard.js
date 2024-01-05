3c8728fccb7395b3d2289116d51cacee1d29dedf
Gabriel Gomes <gabriel.ribeiro.o.g@gmail.com>
2024-01-04 23:37:41 -0300

    improvements on projects css/layout

--- portfolio/projects/static/projects/vue/components/projectCard.js
+++ portfolio/projects/static/projects/vue/components/projectCard.js
@@ -1,18 +1,14 @@
 const ProjectCard = Vue.defineComponent({
   template: `
-    <div class="project-card">
-      <img class="cover-image" :src="project.image_url" />
-      <div class="card-details">
-        <div class="title-row">
-          <div class="text-wrapper">{{ project.title }}</div>
-          <div class="element">$ 1,000,000</div>
-        </div>
-        <p class="div">
-          {{ project.description.slice(0, 125) }}...
-        </p>
-        <div class="date-row">
-          <div class="text-wrapper-2">{{ project.start_date }} to {{ project.end_date || 'Ongoing' }}</div>
-        </div>
+    <img class="cover-image" :src="project.image_url" />
+    <div class="card-details">
+      <div class="title-row">
+        <div class="companion">{{ project.title }}</div>
+        <div class="price">$ 1,000,000</div>
+      </div>
+      <div class="description">{{ project.description.slice(0, 125) }}...</div>
+      <div class="date-row">
+        <div class="date">{{ project.start_date }} to {{ project.end_date || 'Ongoing' }}</div>
       </div>
     </div>
   `,
